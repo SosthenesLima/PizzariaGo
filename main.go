ff40a3daf89eda7fc621104b1496429a5e49ddb4
Sósthenes Oliveira Lima <sosthenes.lima@gmail.com>
2025-08-07 19:23:26 -0300

    Structs03

--- main.go
+++ main.go
@@ -14,6 +14,7 @@ func main() {
 		{ID: 1, nome: "Toscana", preco: 49.50},
 		{ID: 2, nome: "Marguerita", preco: 79.50},
 		{ID: 3, nome: "Atum com queijo", preco: 69.50},
+		{ID: 4, nome: "4Queijos", preco: 80.00},
 	}
 	fmt.Println(pizzas)
 }
