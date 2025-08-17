d69132657c866910134935bdcb322af92562b1bc
Sósthenes Oliveira Lima <sosthenes.lima@gmail.com>
2025-08-17 14:38:07 -0300

    Criando o endpoint

--- main.go
+++ main.go
@@ -13,25 +13,22 @@ type Pizza struct {
 func main() {
 	router := gin.Default()
 	router.GET("/pizzas", getPizzas)
-
-	//toscana, Marguerita, atum com queijo
-	//var pizzas = []Pizza{
-	//	{ID: 1, nome: "Toscana", preco: 49.50},
-	//	{ID: 2, nome: "Marguerita", preco: 79.50},
-	//	{ID: 3, nome: "Atum com queijo", preco: 69.50},
-	//	{ID: 4, nome: "4Queijos", preco: 80.00},
-	//	{ID: 5, nome: "Calabresa", preco: 90.00},
-	//	{ID: 6, nome: "Frango", preco: 78.00},
-	//	{ID: 7, nome: "Carne de Sol", preco: 110.00},
-	//	{ID: 8, nome: "3Queijos", preco: 69.00},
-	//}
-	//fmt.Println(pizzas)
 	router.Run()
 
 }
 
 func getPizzas(c *gin.Context) {
+	var pizzas = []Pizza{
+		{ID: 1, nome: "Toscana", preco: 49.50},
+		{ID: 2, nome: "Marguerita", preco: 79.50},
+		{ID: 3, nome: "Atum com queijo", preco: 69.50},
+		{ID: 4, nome: "4Queijos", preco: 80.00},
+		{ID: 5, nome: "Calabresa", preco: 90.00},
+		{ID: 6, nome: "Frango", preco: 78.00},
+		{ID: 7, nome: "Carne de Sol", preco: 110.00},
+		{ID: 8, nome: "3Queijos", preco: 69.00},
+	}
 	c.JSON(200, gin.H{
-		"pizzas": "Toscana, Calebresa",
+		"pizzas": pizzas,
 	})
 }
