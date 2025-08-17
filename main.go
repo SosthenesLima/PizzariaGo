f69992b37fd68336d387b3e4005349e40c080649
Sósthenes Oliveira Lima <sosthenes.lima@gmail.com>
2025-08-17 14:49:09 -0300

    Criando o endpoint01

--- main.go
+++ main.go
@@ -6,8 +6,8 @@ import (
 
 type Pizza struct {
 	ID    int
-	nome  string
-	preco float64
+	Nome  string
+	Preco float64
 }
 
 func main() {
@@ -19,14 +19,14 @@ func main() {
 
 func getPizzas(c *gin.Context) {
 	var pizzas = []Pizza{
-		{ID: 1, nome: "Toscana", preco: 49.50},
-		{ID: 2, nome: "Marguerita", preco: 79.50},
-		{ID: 3, nome: "Atum com queijo", preco: 69.50},
-		{ID: 4, nome: "4Queijos", preco: 80.00},
-		{ID: 5, nome: "Calabresa", preco: 90.00},
-		{ID: 6, nome: "Frango", preco: 78.00},
-		{ID: 7, nome: "Carne de Sol", preco: 110.00},
-		{ID: 8, nome: "3Queijos", preco: 69.00},
+		{ID: 1, Nome: "Toscana", Preco: 49.50},
+		{ID: 2, Nome: "Marguerita", Preco: 79.50},
+		{ID: 3, Nome: "Atum com queijo", Preco: 69.50},
+		{ID: 4, Nome: "4Queijos", Preco: 80.00},
+		{ID: 5, Nome: "Calabresa", Preco: 90.00},
+		{ID: 6, Nome: "Frango", Preco: 78.00},
+		{ID: 7, Nome: "Carne de Sol", Preco: 110.00},
+		{ID: 8, Nome: "3Queijos", Preco: 69.00},
 	}
 	c.JSON(200, gin.H{
 		"pizzas": pizzas,
