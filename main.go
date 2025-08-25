081422ef2ed940ec9f1e2fb7eed6dcb037cca446
Sósthenes Oliveira Lima <sosthenes.lima@gmail.com>
2025-08-24 21:01:11 -0300

    Criando o endpoint05

--- main.go
+++ main.go
@@ -29,6 +29,7 @@ func getPizzas(c *gin.Context) {
 		{ID: 8, Nome: "3Queijos", Preco: 69.00},
 		{ID: 9, Nome: "Italiana", Preco: 110.00},
 		{ID: 10, Nome: "Chocolate", Preco: 170.00},
+		{ID: 11, Nome: "Banana", Preco: 110.00},
 	}
 	c.JSON(200, gin.H{
 		"pizzas": pizzas,
